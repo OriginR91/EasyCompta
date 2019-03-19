9d22c88741d9b82a3fd9c794e66b09320642b022
OriginR91 <ryad.nouira@gmail.com>
2019-03-19 16:11:58 +0100

    Template

--- User/forms.py
+++ User/forms.py
@@ -5,7 +5,14 @@ class UserForm(forms.ModelForm) :
     class Meta:
         model = User
         widgets = {
-            'password': forms.PasswordInput(),
+            'password': forms.PasswordInput()
+        }
+        labels = {
+            'prenom': 'Prénom',
+            'societe': 'Société',
+            'cp': 'Code postal',
+            'pathlogo': 'Logo',
+            'password': 'Mot de passe'
         }
         fields = ('nom', 'prenom', 'societe', 'adresse', 'cp', 'ville', 'pathlogo', 'pseudo', 'password')
         
