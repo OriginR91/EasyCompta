41be22155cb9bf43f38d292a55321adc9963d636
OriginR91 <ryad.nouira@gmail.com>
2019-03-19 15:19:02 +0100

    MAJ bdd & CSS

--- User/models.py
+++ User/models.py
@@ -7,7 +7,7 @@ class User(models.Model) :
     adresse = models.CharField(max_length=255)
     cp = models.CharField(max_length=20)
     ville = models.CharField(max_length=60)
-    pathlogo = models.TextField()
+    pathlogo = models.CharField(max_length=255)
     pseudo = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
 
